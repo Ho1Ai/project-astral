2d5b3648fd235c2c5fb39a4c95d897a524b3410e
Ho1Ai Arch <koekto33309@gmail.com>
2025-05-11 22:51:31 +0300

    changing profile appearance

--- src/components/workspaceComponents/profile/profileBanner/index.jsx
+++ src/components/workspaceComponents/profile/profileBanner/index.jsx
@@ -1,9 +1,14 @@
 import './index.css'
 
-const ProfileBanner = () => {
+const ProfileBanner = (properties) => {
     return (
-        <main className='profileBanner' style={{background: '#f0c'}}>
-            
+        <main className='profileBanner' style={{background: '#fff', color: "#000", padding: "20px"}}>
+            <h2>
+                {properties.name?properties.name:"User does not exist..."}
+            </h2>
+            <p>
+                {properties.description}
+            </p>
         </main>
     )
 }
--- src/pages/workspacePagesCollector/main/index.jsx
+++ src/pages/workspacePagesCollector/main/index.jsx
@@ -1,13 +1,16 @@
 import WorkspaceHeader from "../../../components/workspaceComponents/workspaceHeader"
 import ProfileBanner from "../../../components/workspaceComponents/profile/profileBanner"
 import ProfileBlog from "../../../components/workspaceComponents/profile/profileBlog"
+import { useState, useEffect } from "react"
 
 const ApplicationProfilePage = () => {
+    let [userInfo,setUserInfo] = useState({uname:'default', description:'static output. Here I place some static content'})
+
     return(<> {/* собрать страницу для главной страницы (ей будет профиль) */}
 
         <WorkspaceHeader />
 
-        <ProfileBanner />
+        <ProfileBanner name={userInfo.uname} description={userInfo.description} />
 
         <ProfileBlog />
 
