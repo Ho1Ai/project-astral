8f42dc337785445d54397632d812dca1d83d42cc
Ho1Ai Arch <koekto33309@gmail.com>
2025-05-17 23:17:42 +0300

    making backups just not to lose progress

--- src/components/workspaceComponents/projects/createProjectModalWindow/index.jsx
+++ src/components/workspaceComponents/projects/createProjectModalWindow/index.jsx
@@ -2,32 +2,43 @@ import { useState } from 'react'
 import './index.css'
 
 const CreateProjectModal = (properties) => {
+    //this page does not work as separated part of this application (so you can't act with it using localhost:5173/tests/modal)
+
     let [linksCount, setLinksCount] = useState([])
+
+    // console.log(typeof(properties.projName[0]), properties.projName[0])
+
     return (
-        <form className='innerPLP_createProjModalWindow' action="#"> 
+        <div className='innerPLP_createProjModalWindow'> 
+
+            {/* I don't need this headache with forms at the moment. Buttons are just sending/submitting this form so I don't wanna fix this stuff at the moment. It won't be a problem... if I had no close buttons (it thinks I need to send/submit it) */}
 
             <div className="innerPLP_headerContainer">
                 <h2>
                     Create New Project
                 </h2>
                 
-                <button className='innerPLP_closeModalWindow'>X</button>
+                <button onClick={() => {properties.closeWindow()}} className='innerPLP_closeModalWindow'>X</button>
             </div>            
             
             <p>
                 Project Name:
             </p>
-            <input required placeholder='e.g. Project Astral' type="text" name='project_name' className='innerPLP_createProjInput_projName innerPLP_createProjInput' /> {/* I don't even know what first classname was supposed to do, btw */}
+            <input onChange={(event)=>properties.projName[1](event.target.value)} value={properties.projName[0]} required placeholder='e.g. Project Astral' type="text" name='project_name' className='innerPLP_createProjInput_projName innerPLP_createProjInput' /> {/* I don't even know what first classname was supposed to do, btw */}
             
-            <p>
-                Description:
-            </p>
-            <textarea style={{resize:"vertical"}} placeholder='Place here some info about your project. For example, you can place main purpose of your project' type="text" className='innerPLP_createProjInput'/>
+            {properties.newProjNameOK && <p style={{color: '#f00', fontSize: '12px'}}>This field is required</p>}
 
             <p>
-                Author Team:
+                Description (Not required):
             </p>
-            <input type="text" placeholder='e.g. Collapse Open-Source Team' className='innerPLP_createProjInput' />
+            <textarea style={{resize:"vertical"}} onChange={(event)=>properties.projDesc[1](event.target.value)} value={properties.projDesc[0]} placeholder='Place here some info about your project. For example, you can place main purpose of your project' type="text" className='innerPLP_createProjInput'/>
+
+            <p>
+                Author Team (Or author's name):
+            </p>
+            <input required type="text" value={properties.projTeam[0]} onChange={event => properties.projTeam[1](event.target.value)} placeholder='e.g. Collapse Open-Source Team' className='innerPLP_createProjInput' />
+
+            {properties.newProjTeamOK && <p style={{color: '#f00', fontSize: '12px'}}>This field is required</p>}
 
             {/* <p>
                 Authors List:
@@ -43,26 +54,26 @@ const CreateProjectModal = (properties) => {
             <p>
                 GitHub:
             </p>
-            <input type="text" placeholder='e.g. https://github.com/Ho1Ai/project-astral' className='innerPLP_createProjInput' />
+            <input onChange={event => properties.projGH[1](event.target.value)} value={properties.projGH[0]} type="text" placeholder='e.g. https://github.com/Ho1Ai/project-astral' className='innerPLP_createProjInput' />
 
             <p>
                 YouTube:
             </p>
-            <input type="text" placeholder='e.g. https://youtube.com/...' className='innerPLP_createProjInput' />
+            <input type="text" onChange={event => properties.projYT[1](event.target.value)} value={properties.projYT[0]} placeholder='e.g. https://youtube.com/...' className='innerPLP_createProjInput' />
 
             <p>
                 Project web site:
             </p>
-            <input type="text" placeholder='e.g. https://project-astral.org/' className='innerPLP_createProjInput' />
+            <input type="text" onChange={event => properties.projWS[1](event.target.value)} value={properties.projWS[0]} placeholder='e.g. https://project-astral.org/' className='innerPLP_createProjInput' />
 
 
             <p>
                 Patreon:
             </p>
-            <input type="text" placeholder='e.g. https://patreon.com/...' className='innerPLP_createProjInput innerPLP_projectLinks' />
+            <input type="text" onChange={event => properties.projPatreon[1](event.target.value)} value={properties.projPatreon[0]} placeholder='e.g. https://patreon.com/...' className='innerPLP_createProjInput innerPLP_projectLinks' />
             
-            <button className='innerPLP_createProjButton'>Create</button>
-        </form>
+            <button onClick={properties.createNewProj} className='innerPLP_createProjButton'>Create</button>
+        </div>
     )
 }
 
--- src/components/workspaceComponents/projects/projectsList/index.jsx
+++ src/components/workspaceComponents/projects/projectsList/index.jsx
@@ -2,39 +2,126 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 
 import ProjectsListInstance from "./projectsListInstance"
+import CreateProjectModal from "../createProjectModalWindow"
 
 import './index.css'
 
 const ProjectsList = () => {
+    
+    //variable declaration
+
     let user = "Ho1Ai" //cork
     let [projectsInfoList, setProjectsInfoList] = useState([
         {
-        name: 'Project Astral',
-        project_link: '/indev/projects/projectInfo?project-astral', //it is in development. Now it will open project page just using link, but in the end it will use projects name
-        "project-name":"project-astral"
-    }
-])
+            name: 'Project Astral',
+            project_link: '/indev/projects/projectInfo?project-astral', //it is in development. Now it will open project page just using link, but in the end it will use projects name
+            "project-name":"project-astral"
+        }
+    ])
+    let [modalWindowState, setModalWindowState] = useState(false)
+
+
+
+    // SPECIAL: input handlers
+    
+    let [newProjName, newProjNameHandler] = useState('')
+    let [newProjDesc, newProjDescHandler] = useState('')
+    let [newProjTeam, newProjTeamHandler] = useState('')
+    let [newProjGH, newProjGHHandler] = useState('')
+    let [newProjYT, newProjYTHandler] = useState('')
+    let [newProjWS, newProjWSHandler] = useState('')
+    let [newProjPatreon, newProjPatreonHandler] = useState('')
+
+    
+    // SPECIAL 2: errors visibility status
+
+    let [newProj_nameErrVisible, setNameErrVisible] = useState(false)
+    let [newProj_teamErrVisible, setTeamErrVisible] = useState(false)
+
+
+    //fetching data from API 
 
     useEffect(()=>{
         axios.get(`http://localhost:8000/api/projects/get-available-projects?username=${user}`).then(res => setProjectsInfoList(res.data))
     }, [])
 
+
+
+    //basic functions
+
+    let errors = false
+
     const createProject = () => {
-        setProjectsInfoList((old) => {
-            return([...old, {name: 'test',project_link: 'test'}])
-        })      
+        if(newProjName == ''){
+            setNameErrVisible(true)
+            errors = true
+        } else {
+            setNameErrVisible(false)
+            errors = false
+        }
+
+        // console.log(newProjName == '')
+
+        if(newProjTeam == '') {
+            setTeamErrVisible(true)
+            errors = true
+        } else {
+            setTeamErrVisible(false)
+            errors = false
+        }
+
+        // if(newProj_nameErrVisible == false && newProj_teamErrVisible == false) {
+        if(errors == false) {
+            axios.post('http://localhost:8000/api/projects/create-new-project', {
+                name: newProjName,
+                description: newProjDesc,
+                authorTeam: newProjTeam,
+                authorsList: [], //it will stay empty at the moment
+                /*projectLinks: {
+                    'GitHub': newProjGH?newProjGH:null,
+                    'YouTube':newProjYT?newProjYT:null,
+                    'WebSite':newProjWS?newProjWS:null, //not a good idea. I have an idea how to make it a lil bit more complex, but not now...
+                    'Patreon':newProjPatreon?newProjPatreon:null
+                }*/
+            }).then(res => {setProjectsInfoList(res.data)
+                console.log(res)
+            })
+        }
+
+        // setProjectsInfoList((old) => {
+        //     return([...old, {name: 'test',project_link: 'test'}])
+        // })      
     }
+
+    const openCreateProjForm = () => setModalWindowState(true)
+
+    const closeProjCreationForm = () => setModalWindowState(false)
 
     return (
         <section className="projectsList">
-            <button onClick={createProject} className="innerPLP_appendProjButton">Create New Project</button>
+            {modalWindowState && <CreateProjectModal 
+            newProjNameOK = {newProj_nameErrVisible}
+            newProjTeamOK = {newProj_teamErrVisible}
+
+            projName = {[newProjName, newProjNameHandler]}
+            projDesc = {[newProjDesc, newProjDescHandler]}
+            projTeam = {[newProjTeam, newProjTeamHandler]}
+            projGH = {[newProjGH, newProjGHHandler]}
+            projYT = {[newProjYT, newProjYTHandler]}
+            projWS = {[newProjWS, newProjWSHandler]}
+            projPatreon = {[newProjPatreon, newProjPatreonHandler]}
+
+            closeWindow = {closeProjCreationForm}
+            createNewProj = {createProject}/>}
+
+            <button onClick={openCreateProjForm} disabled={modalWindowState} className="innerPLP_appendProjButton">Create New Project</button>
             {/* PLP = Project List Page */}
             { 
                 projectsInfoList ? projectsInfoList.map((value, index) => { 
-                    console.log(value)
-                    console.log(Object.keys(projectsInfoList))
+                    // console.log(value)
+                    // console.log(Object.keys(projectsInfoList))
                     return(
-                        <ProjectsListInstance key={index} name={value.name} link={value.project_link}/>
+                        <ProjectsListInstance key={index} does_work = {modalWindowState} name={value.name} link={value.project_link}/>
                     )
                 }) : //<p>There is no projects yet, but, we hope, they will appear here soon :D</p>
                 console.log('no projects yet. Leaving projects list with no instances')
